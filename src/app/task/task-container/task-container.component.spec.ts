2c254b49de9ac3fcbccce8fe8ad259b44e0233aa
Pedro <russopedro87@gmail.com>
2022-02-02 19:09:43 -0300

    'fixing' unit test

--- src/app/task/task-container/task-container.component.spec.ts
+++ src/app/task/task-container/task-container.component.spec.ts
@@ -1,22 +1,31 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { RouterTestingModule } from '@angular/router/testing';
 import { TaskContainerComponent } from './task-container.component';
+import { StoreModule } from '@ngrx/store';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 
-describe('TaskContainerComponent', () => {
+fdescribe('TaskContainerComponent', () => {
   let component: TaskContainerComponent;
   let fixture: ComponentFixture<TaskContainerComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TaskContainerComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [TaskContainerComponent],
+        imports: [
+          IonicModule.forRoot(),
+          RouterTestingModule,
+          StoreModule.forRoot({}),
+        ],
+        providers: [provideMockStore()],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(TaskContainerComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(TaskContainerComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
--- src/app/task/task-page/task.page.spec.ts
+++ src/app/task/task-page/task.page.spec.ts
@@ -1,22 +1,28 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
+import { StoreModule } from '@ngrx/store';
+import { provideMockStore, MockStore } from '@ngrx/store/testing';
 import { TaskPage } from './task.page';
 
 describe('TaskPage', () => {
   let component: TaskPage;
   let fixture: ComponentFixture<TaskPage>;
+  let store: MockStore;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TaskPage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [TaskPage],
+        imports: [IonicModule.forRoot(), StoreModule.forRoot({})],
+        providers: [provideMockStore({})],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(TaskPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      store = TestBed.inject(MockStore);
+      fixture = TestBed.createComponent(TaskPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
